220781247f6bd16c6dc31fc7d3d95dd47bdd1d56
Ëvgenii Golbraikh <evgeniigolbraikh@gmail.com>
2022-07-01 13:56:48 +0300

    Day2 finished

--- Day2/script.js
+++ Day2/script.js
@@ -1,25 +1,40 @@
 let input = document.querySelector("#input");
 input = input.textContent.split("\n    ");
 
-let increaseCounter = 0;
+let forwardCounter = 0
+let downCounter = 0;
+let directions = [, , "up"];
+
 
 // Part 1
-for (let i = 1; i < input.length; i++) {
-  if (Number(input[i]) > Number(input[i - 1])) {
-    increaseCounter += 1;
+for (let i = 0; i < input.length; i++) {
+  let [direction, units] = input[i].split(" ");
+  if (direction === "down") {
+    downCounter += Number(units);
+  } else if (direction === "up") {
+    downCounter -= Number(units);
+  } else {
+    forwardCounter += Number(units);
   }
 }
 
-console.log(increaseCounter)
+console.log(forwardCounter * downCounter)
 
 // Part 2
-increaseCounter = 0;
-for (let i = 3; i < input.length; i++) {
-  let x = Number(input[i]) + Number(input[i - 1]) + Number(input[i - 2]);
-  let y = Number(input[i - 1]) + Number(input[i - 2]) + Number(input[i - 3]);
-  if (x > y) {
-    increaseCounter += 1;
+forwardCounter = 0
+let aimCounter = 0;
+let depthCounter = 0;
+
+for (let i = 0; i < input.length; i++) {
+  let [direction, units] = input[i].split(" ");
+  if (direction === "down") {
+    aimCounter += Number(units);
+  } else if (direction === "up") {
+    aimCounter -= Number(units);
+  } else {
+    forwardCounter += Number(units);
+    depthCounter += (aimCounter * Number(units));
   }
 }
 
-console.log(increaseCounter)
+console.log(forwardCounter * depthCounter)
